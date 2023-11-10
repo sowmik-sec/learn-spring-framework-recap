de66ca76b5e0af4c7a132e05466e31ffbe3812ab
sowmik-sec <sowmik.sec@gmail.com>
2023-11-10 18:45:23 +0600

    bringing in spring framework to make java app loosely coupled

--- src/main/java/com/sowmik/learnspringframeworkrecap/AppGamingBasicJava.java
+++ src/main/java/com/sowmik/learnspringframeworkrecap/AppGamingBasicJava.java
@@ -8,9 +8,11 @@ import com.sowmik.learnspringframeworkrecap.game.SuperContraGame;
 public class AppGamingBasicJava {
     public static void main(String[] args) {
         //var game = new MarioGame();
-        //var game = new SuperContraGame();
+        //var game = new SuperContraGame(); //1: Object Creation
         var game = new PacmanGame();
         var gameRunner = new GameRunner(game);
+        //2: Object Creation + Wiring of Dependencies
+        // Game is a Dependency of GameRunner
         gameRunner.run();
     }
 }
